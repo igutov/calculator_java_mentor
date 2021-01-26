af3b83472dddd9fc65de2b49ece73adc9e9548da
Igutov <serbskat@gmail.com>
2021-01-26 23:51:53 +0400

    first

---
+++ src/calculator_java_mentor/calculator.java
@@ -0,0 +1,12 @@
+package calculator_java_mentor;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class calculator {
+	public static void main(String[] args){
+		System.out.println('2');
+	}
+
+}
