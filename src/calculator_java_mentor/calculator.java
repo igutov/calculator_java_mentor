671bed522c17b5cdb29bd5051580247dbfea0954
Igutov <serbskat@gmail.com>
2021-01-27 01:05:27 +0400

    mask

--- src/calculator_java_mentor/calculator.java
+++ src/calculator_java_mentor/calculator.java
@@ -1,12 +1,29 @@
 package calculator_java_mentor;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class calculator {
-	public static void main(String[] args){
-		System.out.println('2');
+	public static void main(String[] args) {
+
+		// Scanner in = new Scanner(System.in);
+
+		// System.out.println("Введите данные: ");
+
+		// String data = in.nextLine();
+
+		// // String check_data = ("(\\+*)\\d{11}");
+
+		// Pattern pattern = Pattern.compile("\\d+(\\s*|\\+|-|\\*|/|\\s*)\\d+"); //
+		// правила поиска рег выр
+		// Matcher matcher = pattern.matcher(data);
+
+		// while (matcher.find())
+		// System.out.println(matcher.group());
+
+		System.out.println("56 ".matches("\\d{2}\\s"));
+		System.out.println("5 + 4".matches("\\d{1,2}\\s(\\+|-|\\*|/|)\\s\\d{1,2}"));
 	}
 
 }
